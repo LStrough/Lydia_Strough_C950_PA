b8a134a1212ee9c4877249f76e4429a100f20be7
Lydia Strough <strough.lydia@gmail.com>
2022-12-04 09:48:12 -0700

    main.py outline created

---
+++ main.py
@@ -0,0 +1,14 @@
+import HashTable
+
+
+def load_packages(myHash):
+    pass
+
+
+def main_module():
+    myHash = HashTable.ChainingHashTable()
+    load_packages(myHash)
+
+
+if __name__ == '__main__':
+    main_module()
